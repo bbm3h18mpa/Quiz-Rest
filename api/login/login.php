abf8f165a9d50f40d55bbbb12255444ce1ae237a
Nicolas Klein <nicolas.klein@edu.bib.de>
2022-02-10 23:26:32 +0100

    Update login.php

--- api/login/login.php
+++ api/login/login.php
@@ -9,12 +9,4 @@ if($method == 'GET'){
         echo json_encode(['message' => 'User Not Found.']);
     }
     
-} elseif($method == 'POST'){
-    if ($_POST != null) {
-        extract($_POST);
-        $data = Database::execute("SELECT * FROM $apiPath WHERE username=:username and password=:password", array(':username' => $username, ':password' => $password));
-        echo json_encode(['message' => 'User Found.', 'success' => true, 'user' => $data[0]]);
-    } else {
-        echo json_encode(['message' => 'User Not Found', 'success' => false]);
-    }
 }
