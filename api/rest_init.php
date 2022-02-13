3ba9a1ac024fabe28a394b63e9d53e43a6b42d9f
Florian <flo.panten@icloud.com>
2022-02-13 15:14:56 +0100

    A different approach

---
+++ api/rest_init.php
@@ -0,0 +1,5 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT');
+
+$method = $_SERVER['REQUEST_METHOD'];
--- index.php
+++ index.php
@@ -1,28 +1,5 @@
 <?php
-
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, DELETE, PUT");
-
-$method = $_SERVER["REQUEST_METHOD"];
-
-$request_uri = $_SERVER["REQUEST_URI"];
-
-$apiPaths = ["posts"];
-$url = rtrim($request_uri, "/");
-$url = filter_var($request_uri, FILTER_SANITIZE_URL);
-$url = explode("/", $url);
-
-$path = (string) $url[3];
-
-if ($url[4] != null) {
-	$id = (int) $url[4];
-} else {
-	$id = null;
-}
-
-if (in_array($path, $apiPaths)) {
-	include_once "./classes/Database.php";
-	include_once "./api/posts.php";
-} else {
-	echo json_encode(["message" => "Table does not exists"]);
+if (!isset($_POST["session_key"])) {
+	header("Location: login.php");
+	return;
 }
