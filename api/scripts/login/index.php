8b1c638a601b9a60563c0a61dc63a909b45a005d
Nicolas Klein <nicolas.klein@edu.bib.de>
2022-02-16 13:05:06 +0100

    added login

---
+++ api/scripts/login/index.php
@@ -0,0 +1,17 @@
+<?php
+require "../../classes/Database.php";
+require "${_SERVER["DOCUMENT_ROOT"]}/api/rest_init.php";
+/** @var string $method */ //for better code highlighting in PhpStorm
+
+if ($method == 'GET') {
+    extract($_POST);
+    $salt = Database::execute('select salt from "USER" where id = :id', array(":id" => $user_id))[0]["SALT"];
+    PasswordManager::hashFromString($new_password, $salt);
+    $data = Database::execute('SELECT * FROM "USER" WHERE username=:username and password=:password', array(':username' => $username, ':password' => $password));
+    if ($data != null) {
+        echo json_encode($data[0]);
+    } else {
+        echo json_encode(['message' => 'User Not Found.']);
+    }
+
+}
